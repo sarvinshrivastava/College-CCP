e10382ea3d08896e272756505a4aab63cadb9e51
Sarvin Shrivastava <41331485+sarvinshrivastava@users.noreply.github.com>
2023-06-17 18:35:09 +0530

    Create W8 P3.c

---
+++ W8 P3.c
@@ -0,0 +1,19 @@
+/*
+Question
+Multiplication of a number with its complement
+*/
+
+#include <stdio.h>
+
+int main() {
+    int testcases;
+    scanf("%d", &testcases);
+    for(int i = 0; i < testcases; i++) {
+        int num, k;
+        scanf("%d%d", &num, &k);
+        
+        printf("%d\n", num * (num >> k));
+    }
+    
+    return 0;
+}
